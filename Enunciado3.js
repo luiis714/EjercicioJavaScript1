4f120834fc8647ddf164f8ccddcf09eae5d7dece
Luis <luis.mangas15@gmail.com>
2021-04-27 17:06:47 +0200

    Enunciado3 acabado. Muestra un mensaje distinto dependiendo del botón que pulse.

---
+++ Enunciado3.js
@@ -0,0 +1,21 @@
+function pulsaBtn(n){
+    document.getElementById("muestra_resultado").innerHTML = "Se ha pulsado el botón " + n;
+    /*//Este bloque por si quieres hacer diferentes funciones dependiendo del botón pulsado
+    switch(n){
+        case 1:
+            document.getElementById("muestra_resultado").innerHTML = "Se ha pulsado el botón 1";
+            //document.write("Se ha pulsado el botón 1");
+        break;
+        case 2:
+            document.getElementById("muestra_resultado").innerHTML = "Se ha pulsado el botón 2";
+            //document.write("Se ha pulsado el botón 2");
+        break;
+        case 3:
+            document.getElementById("muestra_resultado").innerHTML = "Se ha pulsado el botón 3";
+            //document.write("Se ha pulsado el botón 3");
+        break;
+        default:
+            document.getElementById("muestra_resultado").innerHTML = "No se que botón se ha pulsado";
+            //document.write("No se que botón se ha pulsado");
+    }*/
+}
