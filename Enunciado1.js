10e6e7d7ab26051ca5b65a99ea0e8887ac940d2e
Luis <luis.mangas15@gmail.com>
2021-04-27 16:22:31 +0200

    Enunciado1 acabado. Muestra por pantalla si un número es par o impar.

---
+++ Enunciado1.js
@@ -0,0 +1,9 @@
+var num = prompt("Escribe un número entero: ");
+
+//Si el resto de dividir entre 2 es 0, el número es PAR. Sino es IMPAR
+if((num%2) === 0){
+    alert("El número introducido es PAR");
+}
+else{
+    alert("El número introducido es IMPAR");
+}
